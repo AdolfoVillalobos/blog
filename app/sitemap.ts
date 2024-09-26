ceab0e5cbf0462b23c9fb4ebb3450f82ce92c767
Adolfo Villalobos <amvillalobos@uc.cl>
2024-09-26 20:21:12 +1000

    feat: add project route to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { getBlogPosts } from 'app/blog/utils'
-
+import { getProjectPosts } from 'app/project/utils'
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
@@ -8,10 +8,16 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  let projects = getProjectPosts().map((project) => ({
+    url: `${baseUrl}/project/${project.slug}`,
+    lastModified: project.metadata.publishedAt,
+  }))
+
+  let routes = ['', '/blog', '/project'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+
+  return [...routes, ...blogs, ...projects]
 }
