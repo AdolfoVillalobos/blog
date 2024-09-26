8807173b0e9faa3f47643835f22811bd6787c03f
Adolfo Villalobos <amvillalobos@uc.cl>
2024-09-26 19:41:17 +1000

    feat: add Google Analytics Id

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import { BlogPosts } from 'app/components/posts'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+    <>
+      <GoogleAnalytics gaId="G-4K5RHNCVMZ" />
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Adolfo's Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm Adolfo, a software engineer and data scientist from Chile living in Melbourne, Australia.
-       
-        `}
+        {`I'm Adolfo Villalobos, a software engineer and data scientist from `}
+        <img src="https://cdn-icons-png.flaticon.com/512/197/197586.png" width="13" alt="Chilean flag" style={{ display: 'inline', verticalAlign: 'middle', marginRight: '4px' }} />
+        {`Chile living in Melbourne, `}
+        <img src="https://cdn-icons-png.flaticon.com/512/323/323367.png" width="13" alt="Australian flag" style={{ display: 'inline', verticalAlign: 'middle', marginRight: '4px' }} />
+        {`Australia.`}
       </p>
       <p className="mb-4">
-        {`I currently work as an Algorithmic Trader at a hedge fund, where I focus on
+        {`I currently work as an Algorithmic Trader at a Cryptocurrency Exchange called Orionx.com, where I focus on
         high-frequency trading and quantitative investing. In my free time, I 
         enjoy working on interesting problems and building products that can potentially
         make a difference.
@@ -25,5 +31,6 @@ export default function Page() {
         <BlogPosts />
       </div>
     </section>
+    </>
   )
 }
