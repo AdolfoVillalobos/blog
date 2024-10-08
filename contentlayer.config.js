cb41efbb3ef43015f19e2c194c3265efdf435402
Adolfo Villalobos <amvillalobos@uc.cl>
2024-10-08 19:01:12 +1100

    feat: mdx highlights

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,5 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
@@ -67,5 +70,41 @@ export const Post = defineDocumentType(() => ({
 	computedFields,
 }));
 
-export default makeSource({ contentDirPath: './content', documentTypes: [Post, Project], mdx: { remarkPlugins: [remarkGfm],
-} })
+export default makeSource({
+	contentDirPath: "./content",
+	documentTypes: [Post, Project],
+	mdx: {
+		remarkPlugins: [remarkGfm],
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypePrettyCode,
+				{
+					theme: "github-dark",
+					onVisitLine(node) {
+						// Prevent lines from collapsing in `display: grid` mode, and allow empty
+						// lines to be copy/pasted
+						if (node.children.length === 0) {
+							node.children = [{ type: "text", value: " " }];
+						}
+					},
+					onVisitHighlightedLine(node) {
+						node.properties.className.push("line--highlighted");
+					},
+					onVisitHighlightedWord(node) {
+						node.properties.className = ["word--highlighted"];
+					},
+				},
+			],
+			[
+				rehypeAutolinkHeadings,
+				{
+					properties: {
+						className: ["subheading-anchor"],
+						ariaLabel: "Link to section",
+					},
+				},
+			],
+		],
+	},
+});
